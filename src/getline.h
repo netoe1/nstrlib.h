a5235a102482c804f6eea7d70732bdf9ce4d4dac
weealDCDEF <elynetobr@outlook.com>
2023-04-19 08:18:20 -0300

    encaps

--- src/getline.h
+++ src/getline.h
@@ -1,70 +1,9 @@
+#ifndef __GETLINE_H_
+#define __GETLINE_H_
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "getline.h"
 
-size_t getline_px0(char **lineptr, size_t *n, FILE *stream)
-{
-    if (lineptr == NULL || n == NULL || stream == NULL)
-    {
-        return -1;
-    }
-
-    size_t len = 0;
-    size_t num_chars = 0;
-    char *line = *lineptr;
-
-    /* Aloca memória inicialmente se lineptr aponta para NULL ou se n é 0 */
-    if (line == NULL || *n == 0)
-    {
-        *lineptr = malloc(128);
-        if (*lineptr == NULL)
-        {
-            return -1;
-        }
-        *n = 128;
-        line = *lineptr;
-    }
-
-    while (1)
-    {
-        int c = fgetc(stream);
-
-        /* Se atingir o final do arquivo ou ocorrer um erro na leitura, retorna o número de caracteres lidos */
-        if (c == EOF)
-        {
-            if (num_chars == 0)
-            {
-                return -1;
-            }
-            break;
-        }
-
-        /* Realoca a memória se a string estiver cheia */
-        if (len >= *n - 1)
-        {
-            *n *= 2;
-            char *new_line = realloc(line, *n);
-            if (new_line == NULL)
-            {
-                return -1;
-            }
-            line = new_line;
-            *lineptr = new_line;
-        }
-
-        /* Adiciona o caractere lido à string */
-        line[len] = c;
-        len++;
-        num_chars++;
-
-        /* Se atingir um caractere de quebra de linha, retorna a string lida */
-        if (c == '\n')
-        {
-            break;
-        }
-    }
-
-    /* Adiciona o caractere nulo à string */
-    line[len] = '\0';
-
-    return num_chars;
-}
+size_t getline_px0(char **lineptr, size_t *n, FILE *stream);
+#endif
---
+++ src/nlibstr.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "getline.h"
+#include "nlibstr.h"
+
+void show_str(char *str)
+{
+    int i;
+    if (str != NULL)
+    {
+        for (i = 0; i < sizeof(str); i++)
+        {
+            if (str[i] == '\0')
+            {
+                printf("\\0");
+            }
+            else
+            {
+                printf("%c", str[i]);
+            }
+        }
+    }
+}
+void cls_buf()
+{
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+}
+int indexof_str(char *string, char character)
+{
+    int i = 0;
+
+    for (i = 0; i < strlen(string); i++)
+    {
+        if (string[i] == character)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+void stringCopy(char *source, char *dest)
+{
+    if (sizeof(source) != sizeof(dest))
+    {
+        printf("\nThe both strings must have the same size!\n");
+        return;
+    }
+
+    int i = 0;
+
+    while (dest[i] != '\0')
+    {
+        dest[i] = source[i];
+        i++;
+    }
+}
+int sizeof_str(char *buf)
+{
+    return sizeof(buf);
+}
+void toupper_str(char *string)
+{
+    int i;
+    for (i = 0; i < sizeof(string); i++)
+    {
+        string[i] = toupper(string[i]);
+    }
+}
+void tolower_str(char *string)
+{
+    int i;
+    for (i = 0; i < sizeof(string); i++)
+    {
+        string[i] = tolower(string[i]);
+    }
+}
+int cmpsizeof_str(char *buf1, char *buf2)
+{
+    if (sizeof(buf1) != sizeof(buf2))
+    {
+        return -1;
+    }
+
+    return 0;
+}
+int cmp_str(char *str1, char *str2)
+{
+    if (str1 != NULL && str2 != NULL)
+    {
+        for (int i = 0; i < strlen(str1); i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                return 0;
+            }
+        }
+    }
+    else
+    {
+        return -1;
+    }
+
+    return 1;
+}
+
+void dynamic_cpystr(char *str_dinamic, char *content)
+{
+    int i;
+    str_dinamic = (char *)malloc(sizeof(str_dinamic));
+
+    str_dinamic[sizeof(str_dinamic)] = '\0';
+    for (i = 0; i < sizeof(str_dinamic); i++)
+    {
+        str_dinamic[i] = content[i];
+    }
+}
